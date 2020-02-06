a89be73fded148cb13a01a63c5d817e1a317659b
Jose Arce y de la Borbolla <joseab10@gmail.com>
2020-02-06 01:50:36 +0100

    Cosmetic changes

--- lib/continuous_reinforce.py
+++ lib/continuous_reinforce.py
@@ -2,16 +2,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 
-from utils import *
+from utils import EpisodeStats, tn, tt, print_stats
 
-from value_functions import *
-from policies import *
 
 class REINFORCE:
-	def __init__(self, policy, action_fun, state_dim, action_dim, gamma, baseline=False, baseline_fun = None, lr=1e-4, bl_lr=1e-4):
-
+	def __init__(self, policy, action_fun, state_dim, action_dim, gamma, baseline=False, baseline_fun=None,
+				 lr=1e-4, bl_lr=1e-4):
 
 		self._pi = policy
 		self._action_fun = action_fun
@@ -39,7 +36,6 @@ class REINFORCE:
 
 		self._pi_optimizer = optim.Adam(self._pi.parameters(), lr=self._learning_rate)
 
-
 	def _get_action(self, s, deterministic=False):
 
 		return self._pi.get_action(s, deterministic=deterministic)
@@ -48,7 +44,8 @@ class REINFORCE:
 		return self._action_fun.act2env(self._get_action(s, deterministic=deterministic))
 
 	def train(self, env, episodes, time_steps, initial_state=None, initial_noise=0.5):
-		stats = EpisodeStats(episode_lengths=np.zeros(episodes), episode_rewards=np.zeros(episodes), episode_loss=np.zeros(episodes))
+		stats = EpisodeStats(episode_lengths=np.zeros(episodes), episode_rewards=np.zeros(episodes),
+							 episode_loss=np.zeros(episodes))
 
 		for e in range(episodes):
 			# Generate an episode.
@@ -72,20 +69,19 @@ class REINFORCE:
 					break
 				s = ns
 
-
 			gamma_t = 1
 			for t in range(len(episode)):
-				# Find the first occurance of the state in the episode
+				# Find the first occurrence of the state in the episode
 				s, a, r = episode[t]
 
-				G = 0
+				g = 0
 				gamma_kt = 1
 				for k in range(t , len(episode)):
 					gamma_kt = gamma_kt * self._gamma
 					_, _, r_k = episode[k]
-					G = G + (gamma_kt) * r_k
+					g = g + (gamma_kt * r_k)
 
-				G = float(G)
+				g = float(g)
 
 				p = self._pi(s, a)
 
@@ -100,9 +96,9 @@ class REINFORCE:
 
 				if self._baseline:
 					bl = self.baseline_fun(s)
-					delta = G - bl
+					delta = g - bl
 
-					bl_loss = self._bl_loss_function(self.baseline_fun(s), tt([G]))
+					bl_loss = self._bl_loss_function(self.baseline_fun(s), tt([g]))
 
 					self._bl_optimizer.zero_grad()
 					bl_loss.backward()
@@ -110,7 +106,7 @@ class REINFORCE:
 
 					score_fun = torch.mean(- (gamma_t * delta) * log_p)
 				else:
-					score_fun = torch.mean(- (gamma_t * G) * log_p)
+					score_fun = torch.mean(- (gamma_t * g) * log_p)
 
 				stats.episode_loss[e] += score_fun.item()
 
