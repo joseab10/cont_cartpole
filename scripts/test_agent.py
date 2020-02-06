9ee9ee4f54fa3a4e01671b76a9fa6308ccda39c2
Jose Arce y de la Borbolla <joseab10@gmail.com>
2020-02-06 02:35:48 +0100

    Restructured as standalone script Replaced old parse functions from test_agent.py

--- scripts/test_agent.py
+++ scripts/test_agent.py
@@ -1,46 +1,47 @@
+import sys
+
 import pickle
 
-from continuous_cartpole import *
-from parameters import parse_reward_function, parse_init_noise, parse_init_state
+from continuous_cartpole import ContinuousCartPoleEnv
 
-from train_agent import test_agent, plot_run_stats
+from train import test_agent, plot_run_stats
 
 import argparse
 
+import reward_functions
+from init import initial_states, initial_noises
 
-if __name__ == "__main__":
+# Added to be able to find python files outside of cwd
+sys.path.append('../lib')
 
-	def_dir = '../save/models'
-	def_exp = 'plot_stats'
+parser = argparse.ArgumentParser()
 
-	parser = argparse.ArgumentParser()
+parser.add_argument('--file', action='store'  , default=''    , help='Path to the model file.', type=str)
 
-	parser.add_argument('--file', action='store'  , default=''    , help='Path to the model file.', type=str)
+parser.add_argument('--ep'    , action='store', default=5     , help='Number of test episodes.', type=int)
+parser.add_argument('--ts'    , action='store', default=500   , help='Time steps per episode.', type=int)
+parser.add_argument('--inist' , action='store', default='none', help='Initial State.', type=str)
+parser.add_argument('--inirnd', action='store', default='none', help='Initial Noise.', type=str)
+parser.add_argument('--rew'   , action='store', default='none', help='Reward Function.', type=str)
 
-	parser.add_argument('--ep'    , action='store', default=5     , help='Number of test episodes.', type=int)
-	parser.add_argument('--ts'    , action='store', default=500   , help='Time steps per episode.', type=int)
-	parser.add_argument('--inist' , action='store', default='none', help='Initial State.', type=str)
-	parser.add_argument('--inirnd', action='store', default='none', help='Initial Noise.', type=str)
-	parser.add_argument('--rew'   , action='store', default='none', help='Reward Function.', type=str)
+parser.add_argument('--smw'   , action='store', default=10    , help='Smoothing window', type=int)
 
-	parser.add_argument('--smw'   , action='store', default=10    , help='Smoothing window', type=int)
+args = parser.parse_args()
 
-	args = parser.parse_args()
+reward_function = getattr(reward_functions, args.rew)
+initial_state = initial_states[args.inist]
+initial_noise = initial_noises[args.inirnd]
 
-	reward_function = parse_reward_function(args.rew, {'time_steps': args.ts})
-	initial_state = parse_init_state(args.inist)
-	initial_noise = parse_init_noise(args.inirnd)
+env = ContinuousCartPoleEnv(reward_function=reward_function)
 
-	env = ContinuousCartPoleEnv(reward_function=reward_function)
+with open(args.file, 'rb') as f:
+	agent = pickle.load(f)
 
-	with open(args.file, 'rb') as f:
-		agent = pickle.load(f)
+stats = test_agent(env, agent, episodes=args.ep, time_steps=args.ts, initial_state=initial_state,
+				   initial_noise=initial_noise, render=True)
 
-	stats = test_agent(env, agent, episodes=args.ep, time_steps=args.ts, initial_state=initial_state,
-					   initial_noise=initial_noise, render=True)
+plt_stats = [{'run': 'test', 'stats': {'rewards': stats.episode_rewards.reshape([1, args.ep]),
+										'lengths': stats.episode_lengths.reshape([1, args.ep])}}]
 
-	plt_stats = [{'run': 'test', 'stats': {'rewards': stats.episode_rewards.reshape([1, args.ep]),
-											'lengths': stats.episode_lengths.reshape([1, args.ep])}}]
-
-	plot_run_stats(plt_stats, plot_runs=True, plot_agg=False, smth_wnd=args.smw,
-				   show=True, save=False)
+plot_run_stats(plt_stats, plot_runs=True, plot_agg=False, smth_wnd=args.smw,
+			   show=True, save=False)
