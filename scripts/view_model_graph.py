8788890f02c8024a20a2b2b58227ddabf996d7c8
Jose Arce y de la Borbolla <joseab10@gmail.com>
2020-02-06 02:34:50 +0100

    Restructured as standalone script

--- scripts/view_model_graph.py
+++ scripts/view_model_graph.py
@@ -1,18 +1,17 @@
+import sys
+
 from continuous_reinforce import REINFORCE
-from policies import *
-from parameters import act_clipcont
-from value_functions import *
+from policies import BetaPolicy, GaussPolicy
+from value_functions import StateValueFunction, StateActionValueFunction
 from neural_networks import MLP
-
-
-
-
-#from torchviz import make_dot
+from action_functions import act_clipcont
 
 from torch.utils.tensorboard import SummaryWriter
 
-
 import torch
+
+# Added to be able to find python files outside of cwd
+sys.path.append('../lib')
 
 state_dim = 4
 action_dim = 1
@@ -27,8 +26,6 @@ pig = GaussPolicy(state_dim, action_dim)
 baseline = StateValueFunction(state_dim)
 agent = REINFORCE(pib, act_clipcont, state_dim, action_dim, 0.99, baseline=True, baseline_fun=baseline)
 
-#g = make_dot(pi(s, a), params=dict(pi.named_parameters()))
-#g.render()
 
 Q = StateActionValueFunction(state_dim, 4)
 Qt = StateActionValueFunction(state_dim, 4)
