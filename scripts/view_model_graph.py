21240694feacebb373987a05da23232b2794fca8
Jose Arce y de la Borbolla <joseab10@gmail.com>
2020-02-06 02:53:39 +0100

    Bug Fixes

--- scripts/view_model_graph.py
+++ scripts/view_model_graph.py
@@ -1,4 +1,9 @@
 import sys
+# Added to be able to find python files outside of cwd
+sys.path.append('../lib')
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
 
 from continuous_reinforce import REINFORCE
 from policies import BetaPolicy, GaussPolicy
@@ -6,12 +11,8 @@ from value_functions import StateValueFunction, StateActionValueFunction
 from neural_networks import MLP
 from action_functions import act_clipcont
 
-from torch.utils.tensorboard import SummaryWriter
 
-import torch
-
-# Added to be able to find python files outside of cwd
-sys.path.append('../lib')
+tb_dir = '../save/tb_graphs'
 
 state_dim = 4
 action_dim = 1
@@ -26,13 +27,18 @@ pig = GaussPolicy(state_dim, action_dim)
 baseline = StateValueFunction(state_dim)
 agent = REINFORCE(pib, act_clipcont, state_dim, action_dim, 0.99, baseline=True, baseline_fun=baseline)
 
-
 Q = StateActionValueFunction(state_dim, 4)
 Qt = StateActionValueFunction(state_dim, 4)
 
-writer = SummaryWriter()
-#writer.add_graph(pib, (s, a))
-#writer.add_graph(pig, (s,a))
-writer.add_graph(Q, s)
-#writer.add_graph(Qt, s)
-writer.close()
+graphs = [
+	{'model': pib, 'inputs': (s, a)},
+	{'model': pig, 'inputs': (s, a)},
+	{'model': Q  , 'inputs': s},
+	{'model': Qt , 'inputs': s}
+]
+
+for graph in graphs:
+
+	writer = SummaryWriter(log_dir=tb_dir)
+	writer.add_graph(Q, s)
+	writer.close()
