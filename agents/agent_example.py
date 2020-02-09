3706ea95034699d90f2591f9f4a436cc41f07742
Jose Arce y de la Borbolla <joseab10@gmail.com>
2020-02-09 17:45:33 +0100

    Changed to it's final form

--- agents/agent_example.py
+++ agents/agent_example.py
@@ -4,49 +4,42 @@ sys.path.append('../lib')
 
 import torch.nn.functional as F
 
+from continuous_reinforce import REINFORCE
 from deep_q_learning import DQN
-from policies import EpsilonGreedyPolicy
-from value_functions import StateActionValueFunction
+from policies import GaussPolicy, BetaPolicy, MlpPolicy, EpsilonGreedyPolicy
+from value_functions import StateValueFunction, StateActionValueFunction
 from action_functions import act_disc2cont
-from reward_functions import rf_info_pos
+from reward_functions import rf_spar_pos, rf_info_pos, rf_info2d_pos
+from schedules import sch_exp_decay, sch_linear_decay, sch_no_decay, Schedule
+from init import initial_states, initial_noises
 from continuous_cartpole import ContinuousCartPoleEnv
 from train import train_agent
-from schedules import sch_exp_decay, sch_linear_decay, sch_no_decay, Schedule
 
 
-file_name = 'ag_dqn_nobuff'
-
-show = False
-
-desc = 'DQN without Replay Buffer'
-runs = 2
-episodes = 100
+runs = 5
+episodes = 5000
 time_steps = 300
 test_episodes = 10
 
 state_dim = 4
 action_dim = 2
 
+# File and output names
+file_name = 'ag_dqn_nobuff'
+desc = 'DQN without Replay Buffer'
+
+# Initial Conditions
+init_state = None
+init_noise = initial_noises['360']
+
+# Value Functions
+q_non_linear    = F.relu
 q_hidden_layers = 1
 q_hidden_dim    = 20
 
-epsilon = 0.2
-
-gamma = 0.99
-doubleQ = True # Run doubleQ-DQN sampling from Q_target and bootstraping from Q
-
-lr = 1e-4
-
-act_fun = act_disc2cont
-reward_fun = rf_info_pos
-
-init_state = None
-init_noise = None
-
-# Value Functions
-Q = StateActionValueFunction(state_dim, action_dim, non_linearity=F.relu, hidden_layers=q_hidden_layers,
+Q = StateActionValueFunction(state_dim, action_dim, non_linearity=q_non_linear, hidden_layers=q_hidden_layers,
 								 hidden_dim=q_hidden_dim)
-Q_target = StateActionValueFunction(state_dim, action_dim, non_linearity=F.relu, hidden_layers=q_hidden_layers,
+Q_target = StateActionValueFunction(state_dim, action_dim, non_linearity=q_non_linear, hidden_layers=q_hidden_layers,
 									hidden_dim=q_hidden_dim)
 
 # Epsilon Schedule
@@ -57,17 +50,30 @@ e1 = 0.10
 decay_fun = sch_exp_decay
 cos_ann = True
 ann_cyc = 5
+
 schedule = Schedule(t0, t1, e0, e1, decay_fun, cosine_annealing=cos_ann, annealing_cycles=ann_cyc)
 
 # Policy
 policy = EpsilonGreedyPolicy(schedule=schedule, value_function=Q)
 
-# Agent
-rb = False
-agent = DQN(policy, act_fun, Q, Q_target, state_dim, action_dim, gamma, doubleQ, reward_fun=reward_fun,
-			replay_buffer=rb, lr=lr)
+# Reward Function
+reward_fun = rf_info2d_pos
 
-# Environment
-env = ContinuousCartPoleEnv(reward_function=reward_fun)
+# Action Pre/Post-Processing Action
+act_fun = act_disc2cont
+
+# Agent
+lr = 1e-4
+gamma = 0.99
+doubleQ = True # Run doubleQ-DQN sampling from Q_target and bootstraping from Q
+rb = True
+rb_max_size = 1e6
+rb_batch_size = 64
+
+agent = DQN(policy, act_fun, Q, Q_target, state_dim, action_dim, gamma, doubleQ, reward_fun=reward_fun,
+			replay_buffer=rb, max_buffer_size=rb_max_size, batch_size=rb_batch_size, lr=lr)
+
+# Training
+show = False
 
 train_agent(agent, desc, file_name, runs, episodes, time_steps, test_episodes, init_state, init_noise, show=show)
