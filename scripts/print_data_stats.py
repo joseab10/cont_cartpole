9fb3b98269907c69b0a92f76837dd673ed64a350
Jose Arce y de la Borbolla <joseab10@gmail.com>
2020-02-10 12:21:15 +0100

    Moved main functionality to utils to be reused by train and test functions

--- scripts/print_data_stats.py
+++ scripts/print_data_stats.py
@@ -7,7 +7,7 @@ import pickle
 
 import argparse
 
-from utils import print_header
+from utils import print_header, print_agg_stats
 
 
 def_dir = '../save/plots'
@@ -24,30 +24,4 @@ with open(args.file, 'rb') as f:
 
 print_header(1, 'Stats from file {}'.format(args.file))
 
-for stat in stats:
-	run = stat['run']
-	run_stats = stat['stats']
-
-	runs = run_stats['rewards'].shape[0]
-	episodes = run_stats['rewards'].shape[1]
-
-	print_header(2, '{}: {} runs x {} episodes'.format(run.title(), runs, episodes))
-
-	avg_rw = np.mean(run_stats['rewards'], axis=0)
-	sdv_rw = np.std(run_stats['rewards'], axis=0)
-	avg_len = np.mean(run_stats['lengths'], axis=0)
-	sdv_len = np.std(run_stats['lengths'], axis=0)
-
-	for agent in range(runs):
-		print_header(3, 'Run {}/{}:'.format(agent, runs))
-		print('Rewards: mean = {}, stddev = {}'.format(agent, avg_rw[agent], sdv_rw[agent]))
-		print('Lengths: mean = {}, stddev = {}'.format(agent, avg_len[agent], sdv_len[agent]))
-
-	avg_rw = np.mean(run_stats['rewards'])
-	sdv_rw = np.std(run_stats['rewards'])
-	avg_len = np.mean(run_stats['lengths'])
-	sdv_len = np.std(run_stats['lengths'])
-
-	print_header(2, 'Global Stats')
-	print('Rewards: mean = {}, stddev = {}'.format(avg_rw, sdv_rw))
-	print('Lengths: mean = {}, stddev = {}'.format(avg_len, sdv_len))
+print_agg_stats(stats)
